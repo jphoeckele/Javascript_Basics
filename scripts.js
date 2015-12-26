e630901990ac10c1e09681f3e955b1c2a34ae63a
jphoeckele <jphoeckele@gmail.com>
2015-12-26 10:32:00 -0500

    commented out prompt messages

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
-var visitorName = prompt("What is your name?");
-alert(visitorName);
+// var visitorName = prompt("What is your name?");
+// alert(visitorName);
+
+// var visitor = prompt("What is your name?");
+// var message = "Hello " + visitor + "! " + "Welcome to my JavaScript examples";
+// message += "I'm so glad that you decided to stop by, ";
+// message += visitor;
+// message += ". Please come back again soon!";
+// document.write(message);
