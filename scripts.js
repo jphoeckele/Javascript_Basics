e51cd3cccd3aced1689688d3d20d639f5a74416f
jphoeckele <jphoeckele@gmail.com>
2015-12-26 11:52:53 -0500

    added mad lib story using prompts.

--- scripts.js
+++ scripts.js
@@ -12,7 +12,20 @@
 // document.write(passphrase.length);
 // console.log(passphrase.length);
 
-var stringToShout = prompt("What should I shout?");
-var shout = stringToShout.toUpperCase();
-shout += "!!!";
-alert(shout);
+// var stringToShout = prompt("What should I shout?");
+// var shout = stringToShout.toUpperCase();
+// shout += "!!!";
+// alert(shout);
+
+var userName = prompt("What is your name?");
+alert("Nice to meet you " + userName);
+alert("We're going to write a short story");
+var location = prompt("Please enter the name of a city.");
+alert("Good Choice!");
+var animal = prompt("Please, enter your favorite type of animal.");
+var animalName = prompt("Enter one more name, please.");
+var day = prompt("Alright, now enter a day of the week.");
+document.write("Once there was a beautiful princess named " + userName + " " + <br>);
+document.write("Princess " + userName + " lived in the magical kindom of " + location + "." + <br>);
+document.write(userName + " had a giant " + animal + " named " + animalName + "." + <br>);
+document.write(animalName + " was so big that she was only fed on " + day + "s.");
